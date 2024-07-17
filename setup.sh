1caec309735eb2669d02231e30bea07435bc3c72
Surely Win Dilag <108412632+Win1817@users.noreply.github.com>
2024-07-17 10:12:12 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --
 
 # Add Kubernetes repository
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Install Kubernetes packages
 sudo apt-get update
